16d827292346c12841fb566856afd0fc0351f5cc
ppm <ppm@wheezy.0x1.com>
2013-09-18 15:27:37 +0530

    HS08TEST

--- codechef/HS08TEST/HS08TEST.c
+++ codechef/HS08TEST/HS08TEST.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
+  int x;
+  double y;
+  double bank_charge = 0.5;
+
+  scanf("%d %lf", &x, &y);
+
+  /* printf("%d %.2f\n", x, y); */
+
+  if (x % 5) {
+    printf("%.2f\n", y);
+    exit(0);
+  }
+
+  if (x + bank_charge <= y) {
+    printf("%.2f\n", y - (x + bank_charge));
+  } else {
+    printf("%.2f\n", y);
+  }
+
   return 0;
 }
--- codechef/HS08TEST/HS08TEST.cc
+++ codechef/HS08TEST/HS08TEST.cc
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
+  int x;
+  double y;
+
+  cin >> x >> y;
+
+  // cout << x << " " << y
+  //      << endl;
+
+  double bank_charge = 0.5;
+
+  cout.precision(2);
+
+  if (x % 5) {
+    cout << fixed << y << endl;
+    exit(0);
+  }
+
+  if (x + bank_charge <= y)
+    cout << fixed << y - (x + bank_charge) << endl;
+  else
+    cout << fixed << y << endl;
+
   return 0;
 }
